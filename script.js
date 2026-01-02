94bb7e4b77ad1d49da3ba8921be3b7e85193480e
Ankit Jha <kaunankitjha@gmail.com>
2026-01-03 00:09:07 +0530

    edited website

---
+++ script.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeBtn = document.getElementsByClassName('close')[0];
+
+document.querySelectorAll('.certificates img').forEach(img => {
+    img.onclick = function() {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+    }
+});
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
